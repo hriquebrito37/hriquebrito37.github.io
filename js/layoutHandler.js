bd7d1467b6cb9231331b75d808772942bf3b1246
hriquebrito37 <68126694+hriquebrito37@users.noreply.github.com>
2020-08-06 23:32:29 -0300

    Create simplified version for mobile

---
+++ js/layoutHandler.js
@@ -0,0 +1,20 @@
+if(document.URL.split("?")[1] === 'm') {
+    if(!document.getElementById('mobile')) {
+        let cssStyle = document.createElement('link');
+        cssStyle.id = 'mobile';
+        cssStyle.rel = 'stylesheet';
+        cssStyle.href = '/css/mobile.css';
+        document.head.appendChild(cssStyle);
+    }
+} else if(!document.getElementsByClassName('scroll')[0]) {
+    let cssStyle = document.createElement('link');
+    cssStyle.className = 'scroll';
+    cssStyle.rel = 'stylesheet';
+    cssStyle.href = '/css/scroll.css';
+    document.head.appendChild(cssStyle);
+
+    let scrollScript = document.createElement('script');
+    scrollScript.className = "scroll"
+    scrollScript.src = '/js/scroll.js';
+    document.body.appendChild(scrollScript);
+}

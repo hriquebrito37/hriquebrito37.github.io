b7671f26ed18fa329a2450ce81db83eb82251845
hriquebrito37 <68126694+hriquebrito37@users.noreply.github.com>
2020-08-06 21:33:20 -0300

    Upgrade scroll animation

--- js/scroll.js
+++ js/scroll.js
@@ -1,8 +1,15 @@
 document.addEventListener("wheel", detectScroll);
 
-// window.onresize = fixSize
-
 let divs = document.querySelectorAll(".scrollblock");
+
+let backgrounds = []
+
+for (let i = 0; i < divs.length; i++) {
+    backgrounds.push(window.getComputedStyle(divs[i]).getPropertyValue("background-color"));
+    divs[i].style.backgroundColor = "rgba(0,0,0,0)";
+}
+
+let canScroll = true;
 
 let currentDiv = 0;
 changeDiv(currentDiv);
@@ -10,37 +17,24 @@ changeDiv(currentDiv);
 const clampNumber = (num, a, b) => Math.max(Math.min(num, Math.max(a, b)), Math.min(a, b));
 
 function detectScroll(ev) {
-    // deltaY > 0 pra baixo
-    // deltaY < 0 pra cima
-    if(ev.deltaY > 0) { currentDiv++; }
-    else if(ev.deltaY < 0) { currentDiv--; }
+    if (canScroll == true) {
+        if(ev.deltaY > 0) { currentDiv++; }
+        else if(ev.deltaY < 0) { currentDiv--; }
+        canScroll = false;
 
-    currentDiv = clampNumber(currentDiv, 0, divs.length - 1);
-    
-    changeDiv(currentDiv);
+        currentDiv = clampNumber(currentDiv, 0, divs.length - 1);
+        
+        changeDiv(currentDiv);
+        setTimeout(function() { canScroll = true; }, 1500);
+    }
 }
 
 
 function changeDiv(divIndex) {
-    divs.forEach(div => { div.style.visibility = 'hidden';});
-    divs[divIndex].style.visibility = 'visible';
+    divs[divIndex].parentElement.style.backgroundColor = backgrounds[divIndex];
 
-    if(divs[divIndex - 1] != undefined) divs[divIndex - 1].children[0].style.left = '-100%'
-    divs[divIndex].children[0].style.left = '0%'
-    // divs[divIndex].children[0].style.width = '100%'
-    if(divs[divIndex + 1] != undefined) {
-        divs[divIndex + 1].children[0].style.left = '100%'
-        // divs[divIndex + 1].children[0].style.width = '0px'
-    }
-}
+    if(divs[divIndex - 1] != undefined) divs[divIndex - 1].style.left = '-100%';
+    if(divs[divIndex + 1] != undefined) divs[divIndex + 1].style.left = '100%';
 
-function fixSize() {
-    var paddingVertical = 50
-    var parentDiv = divs[currentDiv]
-    var childDiv = parentDiv.children[0]
-    if(childDiv.scrollHeight < window.document.children[0].scrollHeight)
-        childDiv.style.height = window.document.children[0].scrollHeight.toString() + "px"
-    else
-        childDiv.style.width = "100%"
-    parentDiv.style.height = (childDiv.scrollHeight + paddingVertical).toString() + 'px';
+    setTimeout(function() { divs[divIndex].style.left = '0%'; }, 1000);
 }
